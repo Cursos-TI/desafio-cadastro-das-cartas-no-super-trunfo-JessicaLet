fb7afda4f74b32e40998ce2c2bc8abaadc73c1d4
JessicaLet <jessicaleticiaalm@gmail.com>
2025-05-29 18:01:59 +0000

    correção

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-struct Carta{
+struct Carta {
     char codigo[4]; //Código cidade (A01, B02)
     int populacao;
     float area;
     float pib;
-    int pontos_turisticos; };
+    int pontos_turisticos;
+};
 
-    int main() {
-
-        struct Carta carta1;
-        struct Carta carta2;
-
+int main() {
+    struct Carta carta1;
+    struct Carta carta2;
 
     //Carta 1
     printf("\n--- Cadastro de cartas ---\n");
@@ -19,50 +18,40 @@ struct Carta{
     printf("Carta 1\n");
     printf("\n");
 
-    printf("Digite o código da carta 1 ");
+    printf("Digite o código da carta 1: ");
     scanf("%s", carta1.codigo);
-    while (getchar() != '\n');
 
-    printf("Digite a população da carta 1 ");
-    scanf("%d", &carta1.populacao);
-    while (getchar() != '\n');
+    printf("Digite a população da carta 1: ");
+    scanf(" %d", &carta1.populacao); 
 
-    printf("Digite a area da carta 1 (Km2) ");
-    scanf("%f", &carta1.area);
-    while (getchar() != '\n');
+    printf("Digite a area da carta 1 em Km2: ");
+    scanf(" %f", &carta1.area);
 
-    printf( "Digite o PIB da carta 1 (trilhões) " );
-    scanf("%f", &carta1.pib);
-    while (getchar() != '\n');
+    printf("Digite o PIB da carta 1 (em trilhões): ");
+    scanf(" %f", &carta1.pib);
 
-    printf("Digite o número de pontos turísticos da carta 1 ");
-    scanf("%d", &carta1.pontos_turisticos);
-    while (getchar() != '\n');
+    printf("Digite o número de pontos turísticos da carta 1: ");
+    scanf(" %d", &carta1.pontos_turisticos);
 
     //Carta 2
     printf("\n");
     printf("-Carta 2-\n");
     printf("\n");
 
-    printf("Digite o código da carta 2 ");
+    printf("Digite o código da carta 2: ");
     scanf("%s", carta2.codigo);
-    while (getchar() != '\n');
 
-    printf("Digite a população da carta 2 ");
-    scanf("%d", &carta2.populacao);
-    while (getchar() != '\n');
+    printf("Digite a população da carta 2: ");
+    scanf(" %d", &carta2.populacao); 
 
-    printf("Digite a area da carta 2 (Km2) ");
-    scanf("%f", &carta2.area);
-    while (getchar() != '\n');
+    printf("Digite a area da carta 2 em Km2: ");
+    scanf(" %f", &carta2.area); 
 
-    printf( "Digite o PIB da carta 2 (trilhões) " );
-    scanf("%f", &carta2.pib);
-    while (getchar() != '\n');
+    printf("Digite o PIB da carta 2 (em trilhões): ");
+    scanf(" %f", &carta2.pib); 
 
-    printf("Digite o número de pontos turísticos da carta 2 ");
-    scanf("%d", &carta2.pontos_turisticos);
-    while (getchar() != '\n');
+    printf("Digite o número de pontos turísticos da carta 2: ");
+    scanf(" %d", &carta2.pontos_turisticos);
 
     //exibição final
     printf("\n--- Dados Cadastrados ---\n");
@@ -84,11 +73,5 @@ struct Carta{
     printf("PIB: %.2f trilhoes\n", carta2.pib);
     printf("Pontos Turisticos: %d\n", carta2.pontos_turisticos);
 
-
-
-
-
-
-   
-
-    return 0; }
+    return 0;
+}
